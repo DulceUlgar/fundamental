da84e99600347ab0fb34c3fe8aa5804ae4d20f24
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-16 09:47:59 +0200

    Update arrayPlusArray.test.js

--- tests/micro-ex/arrayPlusArray.test.js
+++ tests/micro-ex/arrayPlusArray.test.js
@@ -2,21 +2,21 @@ import { describe, it } from "vitest";
 import { arrSum } from '../../src/micro-ex/arrayPlusArray.js';
 
 describe('Fundamentals level 1 - Array Plus Array', () => {
-    it.skip('should return 21', () => {
+    it('should return 21', () => {
         let firstArr = [1, 2, 3];
         let secondArr = [4, 5, 6];
         let result = arrSum(firstArr, secondArr);
         expect(result).toEqual(21);
     });
 
-    it.skip('should return -21', () => {
+    it('should return -21', () => {
         let firstArr = [-1, -2, -3];
         let secondArr = [-4, -5, -6];
         let result = arrSum(firstArr, secondArr);
         expect(result).toEqual(-21);
     });
 
-    it.skip('should return 15', () => {
+    it('should return 15', () => {
         let firstArr = [0, 0, 0];
         let secondArr = [4, 5, 6];
         let result = arrSum(firstArr, secondArr);
