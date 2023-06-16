bb76b815e9d6e8e5f0fa7d704717c3667cd62d74
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-16 09:47:22 +0200

    Update stringExercises.test.js

--- tests/fundamentals-ex/stringExercises.test.js
+++ tests/fundamentals-ex/stringExercises.test.js
@@ -3,37 +3,37 @@ import { concatenateStrings, giveAllLettersInUppercase, giveOneLetter, giveTheIn
 
 describe('String Exercises', () => {
 
-    it.skip('should say Hello', () => {
+    it('should say Hello', () => {
         expect(returnHello()).toBe('Hello');
     });
 
-    it.skip('should return Hello World', () => {
+    it('should return Hello World', () => {
         const helloWorld = "Hello World"
         const result = returnAString(helloWorld);
         expect(result).toBe('Hello World');
     });
 
-    it.skip('should be return I Love JS', () => {
+    it('should be return I Love JS', () => {
         expect(concatenateStrings()).toBe('I Love JS');
     });
 
-    it.skip('should return letter g', () => {
+    it('should return letter g', () => {
         expect(giveOneLetter()).toBe('g');
     });
 
-    it.skip('should return -1', () => {
+    it('should return -1', () => {
         expect(giveTheIndexOfWord()).toEqual(-1);
     });
 
-    it.skip('should return "Give a Man a Pussy Cat" ', () => {
+    it('should return "Give a Man a Pussy Cat" ', () => {
         expect(replaceFishWord()).toBe('Give a Man a Pussy Cat');
     });
 
-    it.skip('should return "RIDE HIM, COWBOY!"', () => {
+    it('should return "RIDE HIM, COWBOY!"', () => {
         expect(giveAllLettersInUppercase()).toBe('RIDE HIM, COWBOY');
     });
 
-    it.skip('should return "Hands Down"', () => {
+    it('should return "Hands Down"', () => {
         expect(removeWhiteSpaces()).toBe('Hands Down');
     });
 });
