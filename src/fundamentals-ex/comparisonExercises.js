f914622ee81bf4fef60e6476ef6a40853337aa07
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-16 09:41:19 +0200

    Update comparisonExercises.js

--- src/fundamentals-ex/comparisonExercises.js
+++ src/fundamentals-ex/comparisonExercises.js
@@ -1,18 +1,22 @@
-export function isJane() {
-    /* La function debe devolver Jane, obligatorio el uso del comparador == */
+export function isJane(name) {
+  /* La function debe devolver Jane, obligatorio el uso del comparador == */    
+  return name;    
+}
 
+export function compareNumbers(number1,number2) {
+  /* La function debe devolver 10, obligatorio el uso del comparador === */
+  if(number1===number2){
+      return 10;
+  }else{
+      return -1;
+  }
 
 }
 
-export function compareNumbers() {
-    /* La function debe devolver 10, obligatorio el uso del comparador === */
-
+export function isGreaterThan10(number) {
+  return number;
 }
 
-export function isGreaterThan10() {
-
-}
-
-export function isGreaterThan10AndEqualTo20() {
-
+export function isGreaterThan10AndEqualTo20(number) {
+  return number;
 }
