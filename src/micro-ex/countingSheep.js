824e3acf86c09eaea552ac7b573ccab00141ccbd
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-16 09:43:10 +0200

    Update countingSheep.js

--- src/micro-ex/countingSheep.js
+++ src/micro-ex/countingSheep.js
@@ -7,5 +7,13 @@ Los "true" son ovejas, los "false" son lobos ... Cuantas ovejas hay?
 
 export function countSheeps(list)
 {
-    
+
+    let count =0; 
+    list.forEach(element => {
+        if(element==true) count++;
+    });
+    if(count==0){
+        return 'UPS!!! Wolfs eaten Sheeps';
+    }
+    return `There are ${count} sheep in total`
 }
