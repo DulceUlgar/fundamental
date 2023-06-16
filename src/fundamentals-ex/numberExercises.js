1134724794505c43be7a06c4b21ccdbee5a64fa1
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-16 09:42:10 +0200

    Update numberExercises.js

--- src/fundamentals-ex/numberExercises.js
+++ src/fundamentals-ex/numberExercises.js
@@ -1,38 +1,49 @@
 
-export function checkVariable(number) {
 
+export function checkVariable(number) {
+    return typeof number;
 }
 
 export function checkIfIsInteger(number) {
-
+    if(typeof number === "integer"){
+        return true;
+    }
+    return false;
 }
 
-export function checkIfNumberIsInfinite() {
+export function checkIfNumberIsInfinite(number) {
     /* Tendrás que utilizar un condicional / if ... */
-
+    if(number === Infinity){
+        return "Number is Infinite";
+    }
+    return false;
 }
 
-export function sum() {
+export function sum(num1,num2) {
     /* Completa la function utilizando el operador de suma + */
-
+    return num1 + num2;
 }
 
-export function subtract() {
+export function subtract(num) {
     /* Completa la function utilizando el operador de sustracción - */
-
+    return 85 - num;
 }
 
-export function multiply() {
+export function multiply(num) {
     /* Completa la function utilizando el operador de sustracción - */
-
+    return num * 5;
 }
 
-export function divide() {
+export function divide(num) {
     /* Completa la function utilizando el operador de división / */
-
+    return num / 2;
 }
 
-export function checkIfNumbIsDivisibleByThree() {
+export function checkIfNumbIsDivisibleByThree(num) {
     /* Completa la function utilizando el operador modulus y utiliza el conditional if() */
-
+    if(num % 3 == 0){
+        return 'Number is divisible by 3';
+    }else{
+        return 'Number isnt divisible by 3'
+    }
 }
