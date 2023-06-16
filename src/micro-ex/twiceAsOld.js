3cebab06a001ae898d025da718abc4414fe30641
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-16 09:44:04 +0200

    Update twiceAsOld.js

--- src/micro-ex/twiceAsOld.js
+++ src/micro-ex/twiceAsOld.js
@@ -13,7 +13,29 @@ padre: 55, hijo: 30 -> 5
 padre: 42, hijo: 21 -> 0
 */
 
-export function twiceAsOld(dadAge, sonAge)
-{
-    
+export function twiceAsOld(dadAge, sonAge) {
+    let auxDadAge = dadAge;
+    let auxSonAge = sonAge;
+    let count = 0;
+
+    let isTwice = (auxSonAge * 2 == auxDadAge);
+    while (isTwice == false && auxSonAge < 100) {
+        auxSonAge++;
+        auxDadAge++;
+        count++;
+        isTwice = (auxSonAge * 2 == auxDadAge);
+    }
+    if (isTwice) return count;
+    auxDadAge = dadAge;
+    auxSonAge = sonAge;
+    count = 0;
+    while (isTwice == false && auxSonAge > 0) {
+        auxSonAge--;
+        auxDadAge--;
+        count++;
+        isTwice = (auxSonAge * 2 == auxDadAge);
+    }
+    if (isTwice) { return count }
+    else { return -1 }
+
 }
